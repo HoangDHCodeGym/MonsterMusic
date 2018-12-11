6811b37884c8349e56fdbe47cee4774f13b25641
lana <sitdownrightnow2552@gmail.com>
2018-12-11 08:53:31 +0700

    add storage service

--- server/src/main/java/com/asynchronousmontser/monstermusic/MonsterMusicApplication.java
+++ server/src/main/java/com/asynchronousmontser/monstermusic/MonsterMusicApplication.java
@@ -1,11 +1,14 @@
 package com.asynchronousmontser.monstermusic;
 
+import com.asynchronousmontser.monstermusic.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class MonsterMusicApplication {
 
     public static void main(String[] args) {
