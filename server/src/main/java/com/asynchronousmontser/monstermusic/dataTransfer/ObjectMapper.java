b5ee1fcac8e3e9e86eed54a38c9b31476d16fb8b
lana <sitdownrightnow2552@gmail.com>
2018-12-15 16:14:46 +0700

    object mapper v1

--- server/src/main/java/com/asynchronousmontser/monstermusic/dataTransfer/ObjectMapper.java
+++ server/src/main/java/com/asynchronousmontser/monstermusic/dataTransfer/ObjectMapper.java
@@ -1,28 +1,39 @@
 package com.asynchronousmontser.monstermusic.dataTransfer;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class ObjectMapper {
+    //TODO fix illegalAccessException.
     public <T> T map(Object object, Class<T> outputType, boolean parseNull) {
-        Object output = new Object();
-        Class<?> inputType = object.getClass();
-        for (Method method : outputType.getDeclaredMethods()) {
-            if (method.getName().matches("^set\\w+$")) {
-                Method setter = method;
-                String fieldName = method.getName().substring(3);
-                try {
-                    Method getter = inputType.getDeclaredMethod("get" + fieldName);
-                    Class<?> returnType = getter.getReturnType();
-                    Object gettedValue = getter.invoke(object);
-                    if (parseNull || gettedValue != null) {
-                        setter.invoke(output, returnType.cast(gettedValue));
+        try {
+            for (Constructor<?> constructor: outputType.getConstructors()){
+                constructor.setAccessible(true);
+            }
+            T output = outputType.newInstance();
+
+            Class<?> inputType = object.getClass();
+            for (Method method : outputType.getDeclaredMethods()) {
+                if (method.getName().matches("^set\\w+$")) {
+                    Method setter = method;
+                    String fieldName = method.getName().substring(3);
+                    try {
+                        Method getter = inputType.getDeclaredMethod("get" + fieldName);
+                        Class<?> returnType = getter.getReturnType();
+                        Object gettedValue = getter.invoke(object);
+                        if (parseNull || gettedValue != null) {
+                            setter.invoke(output, returnType.cast(gettedValue));
+                        }
+                    } catch (Exception e) {
+                        //keep going and left the value null if cannot find setter or getter.
                     }
-                } catch (Exception e) {
-                    //keep going and left the value null if cannot find setter or getter.
                 }
             }
+            return outputType.cast(output);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return outputType.cast(output);
+        return null;
     }
 
 }
