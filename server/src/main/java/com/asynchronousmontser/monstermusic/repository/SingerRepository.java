e05f7d08d3d5658a2aaffba5292c608013865ff1
lana <sitdownrightnow2552@gmail.com>
2018-12-15 09:39:36 +0700

    removed repo annotation

--- server/src/main/java/com/asynchronousmontser/monstermusic/repository/SingerRepository.java
+++ server/src/main/java/com/asynchronousmontser/monstermusic/repository/SingerRepository.java
@@ -5,11 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
-@RepositoryRestResource(
-        collectionResourceRel = "singers",
-        itemResourceRel = "singer",
-        path = "singers"
-)
 public interface SingerRepository extends PagingAndSortingRepository<Singer, Integer> {
 }
--- server/src/main/java/com/asynchronousmontser/monstermusic/repository/SongRepository.java
+++ server/src/main/java/com/asynchronousmontser/monstermusic/repository/SongRepository.java
@@ -2,14 +2,7 @@ package com.asynchronousmontser.monstermusic.repository;
 
 import com.asynchronousmontser.monstermusic.model.Song;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
-@RepositoryRestResource(
-        collectionResourceRel = "songs",
-        itemResourceRel = "song",
-        path = "songs"
-)
+
 public interface SongRepository extends PagingAndSortingRepository<Song,Integer> {
 }
