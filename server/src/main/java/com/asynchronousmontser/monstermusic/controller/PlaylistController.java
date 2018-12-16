6a0bc614bc4ecc3f7961fa5a30a0900eb1ea79fc
lana <sitdownrightnow2552@gmail.com>
2018-12-16 11:25:35 +0700

    basic controller. not tested yet

---
+++ server/src/main/java/com/asynchronousmontser/monstermusic/controller/PlaylistController.java
@@ -0,0 +1,72 @@
+package com.asynchronousmontser.monstermusic.controller;
+
+import com.asynchronousmontser.monstermusic.model.Playlist;
+import com.asynchronousmontser.monstermusic.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("api/playlists")
+public class PlaylistController {
+    private PlaylistService playlistService;
+
+    @Autowired
+    public void setUpPlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    //Basic==================================================
+    @GetMapping
+    public ResponseEntity<Page<Playlist>> getAllPlaylist(Pageable pageable) {
+        Page<Playlist> playlistList = playlistService.findAll(pageable);
+        if (playlistList.isEmpty()) {
+            return ResponseEntity
+                    .noContent()
+                    .build();
+        }
+        return ResponseEntity
+                .ok(playlistList);
+    }
+
+    @PostMapping
+    public ResponseEntity<Playlist> createPlaylist(@RequestBody Playlist playlist,
+                                                   UriComponentsBuilder uriComponentsBuilder) {
+        playlistService.save(playlist);
+        URI uri = uriComponentsBuilder
+                .path("/api/playlists/{id}")
+                .buildAndExpand(playlist.getId())
+                .toUri();
+        return ResponseEntity
+                .created(uri)
+                .body(playlist);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Playlist> getPlaylist(@PathVariable("id") Integer id) {
+        Playlist playlist = playlistService.findOne(id);
+        if (playlist != null) {
+            return ResponseEntity
+                    .ok(playlist);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Playlist> putUpdatePlaylist(@RequestBody Playlist playlist
+            , @PathVariable("id") Integer id) {
+        if (playlistService.findOne(id) != null) {
+            playlist.setId(id);
+            playlistService.save(playlist);
+            return ResponseEntity.ok(playlist);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    //============================================
+
+}
