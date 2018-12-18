e202df99d26baf29cd06beaf0222dcbbc3f4bede
lana <sitdownrightnow2552@gmail.com>
2018-12-18 12:07:14 +0700

    update uri handle in response

--- server/src/main/java/com/asynchronousmontser/monstermusic/controller/FileController.java
+++ server/src/main/java/com/asynchronousmontser/monstermusic/controller/FileController.java
@@ -3,8 +3,6 @@ package com.asynchronousmontser.monstermusic.controller;
 import com.asynchronousmontser.monstermusic.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
