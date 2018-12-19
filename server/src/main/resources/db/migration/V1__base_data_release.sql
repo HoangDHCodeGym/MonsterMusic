67779de2353cada6d6f69011e3defbcab4d3894b
lana <sitdownrightnow2552@gmail.com>
2018-12-19 12:27:54 +0700

    update constrain in database

--- server/src/main/resources/db/migration/V1__base_data_release.sql
+++ server/src/main/resources/db/migration/V1__base_data_release.sql
@@ -9,7 +9,7 @@ CREATE TABLE singer
 (
   id      INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INT(11),
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL
 )
   CHARACTER SET utf8
   COLLATE utf8_unicode_ci;
@@ -19,8 +19,8 @@ CREATE TABLE song
   id        INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id   INT(11),
   singer_id INT(11),
-  FOREIGN KEY (singer_id) REFERENCES singer (id),
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  FOREIGN KEY (singer_id) REFERENCES singer (id) ON DELETE SET NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL
 )
   CHARACTER SET utf8
   COLLATE utf8_unicode_ci;
@@ -29,7 +29,7 @@ CREATE TABLE playlist
 (
   id      INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INT(11),
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL
 )
   CHARACTER SET utf8
   COLLATE utf8_unicode_ci;
@@ -39,8 +39,8 @@ CREATE TABLE playlist_song
   playlist_id INT(11) NOT NULL,
   song_id     INT(11) NOT NULL,
   PRIMARY KEY (playlist_id, song_id),
-  FOREIGN KEY (playlist_id) REFERENCES playlist (id),
-  FOREIGN KEY (song_id) REFERENCES song (id)
+  FOREIGN KEY (playlist_id) REFERENCES playlist (id) ON DELETE SET NULL ,
+  FOREIGN KEY (song_id) REFERENCES song (id) ON DELETE SET NULL
 )
   CHARACTER SET utf8
   COLLATE utf8_unicode_ci;
