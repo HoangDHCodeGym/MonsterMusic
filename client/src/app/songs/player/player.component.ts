a29988824acbf64091b87ba1830720ebc798dd65
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-12-21 11:04:07 +0700

    Add player

--- client/src/app/songs/player/player.component.ts
+++ client/src/app/songs/player/player.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {SongService} from "../../../service/song.service";
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-player',
@@ -11,6 +12,15 @@ export class PlayerComponent implements OnInit {
 
   songId:number;
   currentSongURL: string = '';
+  audio: HTMLAudioElement;
+  interval;
+
+  static timeConverter(time: number): string{
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor( time / 60);
+    return minutes + ':' + seconds;
+  }
+
   constructor(private router: ActivatedRoute,
               @Inject('HOST') private host,
               private songService :SongService) {
@@ -20,15 +30,52 @@ export class PlayerComponent implements OnInit {
     this.router.paramMap.subscribe((paramMap:ParamMap)=>{
       this.songId = Number(paramMap.get('id'));
       this.resolveSongResourceUrl();
-    })
+    });
   }
-  resolveSongResourceUrl(){
+
+  resolveSongResourceUrl() {
     this.songService
       .getSongById(this.songId)
       .subscribe((song)=>{
-        this.currentSongURL = this.host+'/api/files/'+song.link
+        this.currentSongURL = this.host+'/api/files/'+song.link;
+        this.audio = new Audio(this.currentSongURL);
       })
+  }
 
+  clickPlayBtn(): void {
+    const playBtn = $('#playBtn');
+    if (playBtn.hasClass('fa-play')) {
+      this.audio.play();
+      playBtn.addClass('fa-pause');
+      playBtn.removeClass('fa-play');
+      $('#song_duration').text(PlayerComponent.timeConverter(this.audio.duration));
+      this.setCounter();
+    } else {
+      this.audio.pause();
+      playBtn.addClass('fa-play');
+      playBtn.removeClass('fa-pause');
+    }
+  }
+
+  setCounter(): void {
+    const audio = this.audio;
+    const self = this;
+    this.interval = setInterval(function() {
+      $('#running_time').text(PlayerComponent.timeConverter(audio.currentTime));
+      $('#timeLine').val(Math.floor(audio.currentTime / audio.duration * 100));
+      if (audio.currentTime === audio.duration) {
+        clearInterval(self.interval);
+        self.clickPlayBtn();
+      }
+    }, 1000);
+  }
+
+  skipTo(timeLineValue: number): void {
+    this.audio.pause();
+    this.audio.currentTime = Math.floor(this.audio.duration * timeLineValue / 100);
+    clearInterval(this.interval);
+    this.setCounter();
+    this.audio.play();
   }
 
 }
