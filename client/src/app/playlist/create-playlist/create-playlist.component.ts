12f7a8ff5ff4ee6f09a77ca0f68c1a8fb0814741
lana <sitdownrightnow2552@gmail.com>
2018-12-23 11:48:13 +0700

    done add

--- client/src/app/playlist/create-playlist/create-playlist.component.ts
+++ client/src/app/playlist/create-playlist/create-playlist.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PlaylistService} from "../../../service/playlist.service";
 import {PlaylistForm} from "../../../model";
+import {FormGroup, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-create-playlist',
@@ -8,18 +9,39 @@ import {PlaylistForm} from "../../../model";
   styleUrls: ['./create-playlist.component.css']
 })
 export class CreatePlaylistComponent implements OnInit {
-
-  playlistForm: PlaylistForm = {
-    songList :[],
-    description:null,
-    creator:1,
-    name:null,
+  status: number;
+  creMessage: string = '';
+  newPlaylist: PlaylistForm = {
+    songList: [],
+    description: null,
+    creator: 1,
+    name: null,
   };
 
   constructor(private playlistService: PlaylistService) {
   }
 
+  onSubmit(playlistForm: NgForm) {
+    if (playlistForm.valid) {
+      this.playlistService
+        .createPlaylist(this.newPlaylist)
+        .subscribe((playlist) => {
+          if (playlist != null) {
+            this.creMessage = 'success';
+            this.status = 1;
+          } else {
+            this.status = -1;
+            this.creMessage = 'Please try again'
+          }
+        })
+    } else {
+      this.status = -1;
+      this.creMessage = 'Not Valid'
+    }
+  }
+
   ngOnInit() {
+    this.status = 0;
   }
 
 }
